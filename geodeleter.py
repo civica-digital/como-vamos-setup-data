6647ec984d6acb1c2baba58ba5de6db889ba54dd
Ricardo Alanis <ricardo.alanis@gmail.com>
2015-10-30 16:15:12 -0600

    Worked with geodeleter

--- geodeleter.py
+++ geodeleter.py
@@ -1,9 +1,10 @@
 import pandas
 import numpy
-import csv
 
-data = pandas.read_csv("DICCIONARIO_FINAL_V19.csv", delimiter = ";",dtype=numpy.string_, encoding="UTF-8")
+f = "DICCIONARIO_FINAL_V19.csv"
+e = "UTF-8"
+data = pandas.read_csv(f, delimiter = ";",dtype=numpy.string_, encoding=e)
 data = data.drop('GPS', 1)
 data = data.drop('GPS_LA', 1)
 data = data.drop('GPS_LO', 1)
-data.to_csv("output.csv",delimiter=",", index=False)
+data.to_csv("output.csv", delimiter=",", index=False)
