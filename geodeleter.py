03408a757a4a0f283c866fe6e1cb434169e2f9e2
Ricardo Alanis <ricardo.alanis@gmail.com>
2015-11-17 12:26:55 -0600

    added csv prefix to quote_all constant

--- geodeleter.py
+++ geodeleter.py
@@ -1,5 +1,6 @@
 import pandas
 import numpy
+import csv
 
 f = "DICCIONARIO_FINAL_V19.csv"
 e = "UTF-8"
@@ -7,4 +8,4 @@ data = pandas.read_csv(f, delimiter = ";",dtype=numpy.string_, encoding=e)
 data = data.drop('GPS', 1)
 data = data.drop('GPS_LA', 1)
 data = data.drop('GPS_LO', 1)
-data.to_csv("output_1.csv", delimiter=",",dtype=numpy.string_, quoting=QUOTE_ALL, index=False)
+data.to_csv("output_1.csv", delimiter=",",dtype=numpy.string_, quoting=csv.QUOTE_ALL, index=False)
